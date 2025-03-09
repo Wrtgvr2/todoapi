8df900deac3ca55f0ae63670cd2e4cf0c92fa23c
Wrtgvr <saldaniil123@gmail.com>
2025-03-09 22:40:06 +0700

    Test method (no DB connection)

--- api/handlers/users.go
+++ api/handlers/users.go
@@ -1,2 +1,23 @@
-package package models
+package handlers
 
+import (
+	"encoding/json"
+	"net/http"
+	//rep "github.com/wrtgvr/todoapi/repository"
+)
+
+type User struct {
+	Username string   `json:"username"`
+	Todos    []string `json:"todos"`
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	res := []User{ //! Remove before connection to DB
+		{Username: "Wrtgvr", Todos: []string{"1", "2", "10", "41"}},
+		{Username: "TestUser", Todos: []string{}},
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
--- api/router.go
+++ api/router.go
@@ -3,11 +3,14 @@ package api
 import (
 	"net/http"
 
+	"github.com/wrtgvr/todoapi/api/handlers"
 	mws "github.com/wrtgvr/todoapi/api/middlewares"
 )
 
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /users", handlers.GetUsers)
 
 	wrappedMux := mws.ChainMiddlewares(mux,
 		mws.LoggingMiddleware,
