81fbf2651dcead208cf25c195f9af1ecbb086b9f
Wrtgvr <saldaniil123@gmail.com>
2025-03-09 11:38:17 +0700

    Added logging to file

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -1,16 +1,34 @@
 package middlewares
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
+
+var log logrus.Logger
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/favicon.ico" {
-			log.Printf("[%s] %s", r.Method, r.URL.Path)
+			log.Infof("[%s] %s", r.Method, r.URL.Path)
 		}
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+func LogsInit() {
+	log := logrus.New()
+
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   "logs/log.txt",
+		MaxSize:    20,
+		MaxAge:     3,
+		MaxBackups: 5,
+	})
+
+	log.SetFormatter(&logrus.JSONFormatter{})
+	log.SetLevel(logrus.InfoLevel)
 }
