cdc409a83faaa3fc31da27b7062950fc735e485b
Wrtgvr <saldaniil123@gmail.com>
2025-03-11 13:37:03 +0700

    Less useless code

--- app/app.go
+++ app/app.go
@@ -1,36 +1,27 @@
 package app
 
 import (
-	"database/sql"
 	"net/http"
 
-	"github.com/wrtgvr/todoapi/api"
+	"github.com/wrtgvr/todoapi/api/router"
 	"github.com/wrtgvr/todoapi/internal/logger"
 	"github.com/wrtgvr/todoapi/repository"
 )
 
 type App struct {
-	DB     *sql.DB
 	Router http.Handler
 }
 
 func InitApp() (*App, error) {
 	logger.LogsInit()
 
-	db, errDb := repository.OpenDatabase()
+	errDb := repository.OpenDatabase()
 	if errDb != nil {
 		return nil, errDb
 	}
-	router := api.NewRouter()
+	router := router.NewRouter()
 
 	return &App{
-		DB:     db,
 		Router: router,
 	}, nil
-}
-
-func CloseApp() {
-	if err := repository.CloseDatabase(); err != nil {
-		logger.LogError(err)
-	}
 }
