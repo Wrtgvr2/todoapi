97597a257594a321f8a2bebb42679f71db0e7c88
Wrtgvr <saldaniil123@gmail.com>
2025-03-11 21:00:34 +0700

    Added: GET /users handler

--- api/handlers/users.go
+++ api/handlers/users.go
@@ -3,21 +3,21 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
-	//rep "github.com/wrtgvr/todoapi/repository"
-)
 
-type User struct {
-	Username string   `json:"username"`
-	Todos    []string `json:"todos"`
-}
+	"github.com/wrtgvr/todoapi/internal/logger"
+	rep "github.com/wrtgvr/todoapi/repository"
+)
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	res := []User{ //! Remove before connection to DB
-		{Username: "Wrtgvr", Todos: []string{"1", "2", "10", "41"}},
-		{Username: "TestUser", Todos: []string{}},
+	res, err := rep.GetUsers()
+	if err != nil {
+		logger.LogError(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(res)
 }
--- repository/db-users.go
+++ repository/db-users.go
@@ -1,9 +1,34 @@
 package repository
 
 import (
+	_ "github.com/lib/pq"
 	"github.com/wrtgvr/todoapi/models"
 )
 
 func GetUsers() ([]models.User, error) {
-	return nil, nil
+	query := `SELECT id, username FROM users`
+
+	rows, err := DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close() // ??
+
+	var users []models.User
+
+	for rows.Next() {
+		var user models.User
+
+		if err := rows.Scan(&user.ID, &user.Username); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
