b6ba0ce9d80541aef031e03607b760d9af0e1f7d
Wrtgvr <saldaniil123@gmail.com>
2025-03-11 13:27:28 +0700

    Main DB file

--- repository/db-users.go
+++ repository/db-users.go
@@ -1,42 +1,9 @@
 package repository
 
 import (
-	"database/sql"
-	"fmt"
-	"os"
-
-	env "github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+	"github.com/wrtgvr/todoapi/models"
 )
 
-var DB *sql.DB
+func GetUsers() ([]models.User, error) {
 
-func OpenDatabase() (*sql.DB, error) {
-	if err := env.Load(); err != nil {
-		return nil, err
-	}
-
-	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-	)
-
-	var err error
-	DB, err = sql.Open("postgres", dsn)
-
-	if err != nil {
-		return nil, err
-	}
-	if err = DB.Ping(); err != nil {
-		return nil, err
-	}
-
-	return DB, nil
-}
-
-func CloseDatabase() error {
-	return DB.Close()
 }
---
+++ repository/db.go
@@ -0,0 +1,67 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	env "github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func OpenDatabase() error {
+	if err := env.Load(); err != nil {
+		return err
+	}
+
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+	)
+
+	var err error
+	DB, err = sql.Open("postgres", dsn)
+	if err != nil {
+		return err
+	}
+
+	if err := DB.Ping(); err != nil {
+		return err
+	}
+
+	defer DB.Close()
+
+	if err := TablesInit(DB); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TablesInit(DB *sql.DB) error {
+	createTables := `
+	CREATE TABLE IF NOT EXISTS users (
+		id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+		username VARCHAR(24) NOT NULL,
+		password VARCHAR(60) NOT NULL
+	);
+	CREATE TABLE IF NOT EXISTS todos (
+		id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+		user_id BIGINT NOT NULL FOREIGN KEY REFERENCES users(id) ON DELETE CASCADE,
+		title VARCHAR(100) NOT NULL,
+		description TEXT,
+		completed BOOLEAN DEFAULT FALSE,
+		created_at
+	);`
+
+	if _, err := DB.Exec(createTables); err != nil {
+		return err
+	}
+
+	return nil
+}
