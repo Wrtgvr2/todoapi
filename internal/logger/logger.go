e7e5843cf36668db0a06dbc1c2e239df9557dd33
Wrtgvr <saldaniil123@gmail.com>
2025-03-13 18:59:39 +0700

    Logger now create separate files

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,6 +1,11 @@
 package logger
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/sirupsen/logrus"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
@@ -10,8 +15,15 @@ var log *logrus.Logger
 func LogsInit() {
 	log = logrus.New()
 
+	date := time.Now().Format("2006-01-02")
+	filename := fmt.Sprintf("logs/%s-log.txt", date)
+
+	for i := 1; fileExists(filename); i++ {
+		filename = fmt.Sprintf("logs/%s-log-%d.txt", date, i)
+	}
+
 	log.SetOutput(&lumberjack.Logger{
-		Filename:   "logs/log.txt",
+		Filename:   filename,
 		MaxSize:    20,
 		MaxAge:     3,
 		MaxBackups: 5,
@@ -29,4 +41,9 @@ func LogError(err error) {
 	if err != nil {
 		log.Errorf("Error: %v", err)
 	}
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !errors.Is(err, os.ErrNotExist)
 }
