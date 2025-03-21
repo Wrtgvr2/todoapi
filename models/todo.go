1a57c7394fed2bdf7c66e7682ae7764f0485293f
Wrtgvr <saldaniil123@gmail.com>
2025-03-21 20:08:39 +0700

    Added: CreateTodoData struct

--- models/todo.go
+++ models/todo.go
@@ -2,9 +2,15 @@ package models
 
 import "time"
 
+type CreateTodoData struct {
+	UserID      uint64 `json:"user_id"`
+	Title       string `json:"title"`
+	Description string `json:"description,omitempty"`
+}
+
 type Todo struct {
 	ID          uint64    `json:"id"`
-	User_ID     uint64    `json:"userid"`
+	User_ID     uint64    `json:"user_id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	Completed   bool      `json:"completed"`
