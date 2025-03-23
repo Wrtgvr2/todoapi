df417d9923530e0f5dfa34604b2d1fbc4d93171c
Wrtgvr <saldaniil123@gmail.com>
2025-03-23 21:17:52 +0700

    Added: ErrTodoNotFounr error

--- repository/errors.go
+++ repository/errors.go
@@ -9,3 +9,9 @@ type ErrUserNotFound struct{}
 func (e *ErrUserNotFound) Error() string {
 	return "User not found."
 }
+
+type ErrTodoNotFound struct{}
+
+func (e *ErrTodoNotFound) Error() string {
+	return "Todo not found."
+}
