04951b53a954e4f1d020ddeeb0849df28c816f98
Wrtgvr <saldaniil123@gmail.com>
2025-03-25 20:55:14 +0700

    Fixed: UpdateToDoData struct now have ID field

--- models/todo.go
+++ models/todo.go
@@ -3,6 +3,7 @@ package models
 import "time"
 
 type UpdateTodoData struct {
+	ID          uint64 `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Completed   bool   `json:"completed"`
