dfe0cb616a11ebb42e263a41776d820c5beac9ef
Wrtgvr <saldaniil123@gmail.com>
2025-03-30 16:43:19 +0700

    Removed: unused field

--- models/todo.go
+++ models/todo.go
@@ -3,7 +3,6 @@ package models
 import "time"
 
 type UpdateTodoData struct {
-	ID          uint64  `json:"id"`
 	Title       *string `json:"title,omitempty"`
 	Description *string `json:"description,omitempty"`
 	Completed   *bool   `json:"completed,omitempty"`
